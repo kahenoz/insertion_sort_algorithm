85250e0a924384bc79d9171e9749ebad759a9a0a
kahenoz <kahenozz@gmail.com>
2024-06-03 17:49:36 +0300

    while loop is created

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,5 +4,9 @@ def myFunc(A):
     for x in range(1,len(A)):
         key = A[x]
         y = x - 1
+    
+    while y>=0 and A[y]>key:
+            A[y+1] = A[y]
+            y -= 1
 
 array = [31,41,59,26,41,58]
